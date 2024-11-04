bdc6ea49d896c20878eb1a3e6fca3ce23c4761de
Alberto Ziurkelis de Araujo <beto50000za@gmail.com>
2024-11-04 05:54:46 -0300

    Initial Commit

---
+++ Class/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneGroup.Class
+{
+    public class Pedido
+    {
+        public int IdPedido { get; set; }
+        public decimal ValorPedidos { get; set; }
+        public int IdCliente { get; set; }
+        public DateTime DataPedido { get; set; }
+    }
+}
